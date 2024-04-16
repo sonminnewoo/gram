91b68027ad32bd66dce378cc8376b366e4bc7248
SWIN1\sonmi <son>
2024-04-16 16:17:44 +0900

    ㅇ

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,10 +6,10 @@ import images from "../../constants/images";
 const Header = () => (
     <div className="header" style={{
         background: `linear-gradient(rgba(0,0,0,0.7),
-        rgba(0,0,0,0.2), url(${images.header_bg}) center/cover no-repeat)`
+        rgba(0,0,0,0.2)), url(${images.header_bg}) center/cover no-repeat`
     }}>
         <Navbar/>
     </div>
 )
 
-export default Header;
+export default Header;  
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,10 +9,95 @@ const Navbar = () => {
     return (
         <nav className="navbar">
             <div className="container flex">
-                <a href="index.html" alt=""
-                className="navbar__brand">
-                    <img src={images.logo} alt = "sitelogo" />
-                </a>
+                <div className="brand__and__toggler flex">
+                    <a href="index.html" alt=""
+                    className="navbar__brand">
+                        <img src={images.logo} alt = "sitelogo" />
+                    </a>
+                    <button type="button"
+                    className="navbar__open--btntext__light" onClick={() => setToggleMenu(true)}>
+                        <FaBars size={26}/>
+                    </button>
+                </div>
+                <div className="navbar__collapse">
+                    <ul className="navbar__nav">
+                        <li className="nav__item">
+                            <a href="#home" className="nav__link text__upper fw__6
+                            text__light nav__active">Home</a>
+                        </li>
+                        <li className="nav_item">
+                            <a href="#about" className="nav__link text__upper fw__6 
+                            text__light">ABOUT</a>
+                        </li>
+                        <li className="nav_item">
+                            <a href="#signUp" className="nav__link text__upper fw__6 
+                            text__light">SIGNUP</a>
+                        </li>
+                        <li className="nav_item">
+                            <a href="#signIn" className="nav__link text__upper fw__6 
+                            text__light">SIGNIN</a>
+                        </li>
+                        <li className="nav_item">
+                            <a href="#myPage" className="nav__link text__upper fw__6 
+                            text__light">MYPAGE</a>
+                        </li>
+                        <li className="nav_item">
+                            <a href="#place" className="nav__link text__upper fw__6 
+                            text__light">PLACE</a>
+                        </li>
+
+                    </ul>
+                </div>
+
+                {toggleMenu && (
+                    <div className="navbar__smallscreen">
+                        <button type="button"
+                        className="navbar__close--btn text__light"
+                        onClick={() => setToggleMenu(false)}>
+                            <FaTimes size={32} />
+                        </button>
+                        <ul 
+                        className="navbar__nav--smallscreen 
+                        text__light">
+                            <li className="nav__item">
+                                <a href="#home"
+                                className="nav__link
+                                text__upper fw__6
+                                nav__active">Home</a>
+                            </li>
+                            <li className="nav__item">
+                                <a href="#about"
+                                className="nav__link
+                                text__upper fw__6
+                                ">ABOUT</a>
+                            </li>
+                            <li className="nav__item">
+                                <a href="#signUp"
+                                className="nav__link
+                                text__upper fw__6
+                                ">SIGNUP</a>
+                            </li>
+                            <li className="nav__item">
+                                <a href="#signIn"
+                                className="nav__link
+                                text__upper fw__6
+                                ">SIGNIN</a>
+                            </li>
+                            <li className="nav__item">
+                                <a href="#myPage"
+                                className="nav__link
+                                text__upper fw__6
+                                ">MYPAGE</a>
+                            </li>
+                            <li className="nav__item">
+                                <a href="#place"
+                                className="nav__link
+                                text__upper fw__6
+                                ">PLACE</a>
+                            </li>
+                        </ul>
+                    </div>
+                )}
             </div>
         </nav>
     )
