43ce74483c67a6cb9dfe5197cccd0affc862d0b1
SWIN1\sonmi <son>
2024-04-16 14:56:42 +0900

    초기 바탕 완료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from 'react';  // Make sure React is imported
 import './App.css';
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="hplder">
+    <div className="holder">
+      <Header/>
     </div>
   );
 }
